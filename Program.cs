f72d6b342035bf79366a19331aeac72d35bd7b7b
Varghese Charly <vcharly@usf.edu>
2020-09-17 13:42:55 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// Author: Varghese Charly
+// Project Name: Tech Assignment 2b
+// Purpose: The code is designed to take in a integer grade and convert that to a letter grade accourding to specifications on the syllabus.
+
+using System;
+
+namespace Tech_Assign_2b
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
